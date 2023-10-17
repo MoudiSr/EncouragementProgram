850dc847b46da1b6ac9bbd7315c2b40400f4166d
Mohammad Srouji <40596420+MoudiSr@users.noreply.github.com>
2023-10-17 12:07:53 +0000

    add calendar

--- src/BehaivorsPage.jsx
+++ src/BehaivorsPage.jsx
@@ -1,27 +1,42 @@
 import DashboardLayout from "./DashboardLayout"
-import { motion } from "framer-motion"
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
-import { Fragment, useRef, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import {useState} from "react"
+
+const localizer = momentLocalizer(moment);
 
 const BehaivorsPage = () => {
 
-    const [open, setOpen] = useState(false)
+    const [events, setEvents] = useState([ // Initialize events with an empty array
+    {
+        title: 'Event 1',
+        start: new Date(2023, 10, 20, 10, 0),
+        end: new Date(2023, 10, 20, 12, 0)
+    },
+    {
+        title: 'Event 2',
+        start: new Date(2023, 10, 21, 14, 0),
+        end: new Date(2023, 10, 21, 16, 0)
+    },
+    // Add more events as needed
+]);
 
-    const cancelButtonRef = useRef(null)
 
-    const handleDayClick = () => {
-        setOpen(true)
-    }
+   
 
     return (
         <DashboardLayout>
             <h1 className="text-right text-2xl">ملاحظات سلوكية</h1>
-            
+            <Calendar
+                popup
+                selectable
+                localizer={localizer}
+                style={{ height: 600, marginTop: '1rem' }}
+                events={events}
+            />
         </DashboardLayout>
-    )
+    );
 }
 
-export default BehaivorsPage
+export default BehaivorsPage;
